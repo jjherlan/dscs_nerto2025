ba445f125ba04d3b0a4b68602dfe856995584ed3
James J. Herlan <75087560+jjherlan@users.noreply.github.com>
2025-07-10 18:03:54 -0400

    Update dscs_nerto2025_hsdm_v1_ch10_20250709.R
    commit, 1803, 7/10/2025

--- sandbox/dscs_nerto2025_hsdm_v1_ch10_20250709.R
+++ sandbox/dscs_nerto2025_hsdm_v1_ch10_20250709.R
@@ -1,6 +1,7 @@
 library(unmarked)
 library(jagsUI)
 library(R2WinBUGS)
+library(R2OpenBUGS)
 
 # library(mgcv) # Not used in this code snippet, but can be used for GAMs
 
@@ -97,6 +98,210 @@ fm2 <- jags(data = win.data, inits = inits, parameters.to.save = params,
           model.file = "model.txt", n.chains = nc, n.thin = nt, n.burnin = nb,
           n.iter = ni, DIC = TRUE)
 print(fm2, digits = 3)
+
+# 10.4 A slightly more complex site-occupancy model with covariates
+
+# Choose sample sizes and prepare obs. data array y
+set.seed(1) # So we get all the same data
+M <- 100 # number of sites
+J <- 3 # number of presence/absence measurements
+y <- matrix(NA, nrow = M, ncol = J) # to contain the obs. data
+
+# Create a covariate called vegHt
+
+vegHt <- sort(runif(M, -1, 1)) # sort for graphical convenience
+
+# Choose parameter values for occupancy model and compute occupancy
+
+beta0 <- 0 # logit-scale intercept
+beta1 <- 3 # logit-scale slope for vegHt
+psi <- plogis(beta0 + beta1 * vegHt) # occupancy probability
+#plot(vegHt, psi, type = "l", ylim = c(0,1), lwd = 3, xlab = "Vegetation height", ylab = "Occupancy probability") 
+# plot psi relationship
+
+# Now visit each site and observe presence/absence perfectly
+
+z <- rbinom(n = M, size = 1, prob = psi) # occupancy state (true presence/absence)
+
+# Look at data so far
+
+table(z)
+
+# Plot the true system state
+
+par(mfrow = c(1, 3), mar = c(5, 5, 2, 2), cex.axis = 1.5, cex.lab = 1.5)
+
+plot(vegHt, z, xlab = "Vegetation Height", ylab = "True presence/absence (z)", frame = F, 
+     main = "True Occupancy State", pch = 19, cex = 1.5, col = ifelse(z == 1, "blue", "red"))
+
+plot(function (x) plogis(beta0 + beta1*x), -1, 1, add = T, lwd = 3, col = "red")
+
+# Create a covariate called wind
+
+wind <- array(runif(M*J, -1, 1), dim = c(M, J)) # random wind covariate
+
+# Choose parameter values for detection model and compute detection probability
+
+alpha0 <- -2 # logit-scale intercept
+alpha1 <- -3 # logit-scale slope for wind
+
+p <- plogis(alpha0 + alpha1*wind) # detection probability
+
+# plot(p ~ wind, ylim = c(0,1)) # plot detection probability relationship
+
+# Take J = 3 presence/ansence measurements at each site
+
+for (j in 1:J) {
+  y[,j] <- rbinom(M, z, p[,j]) # detection state (presence/absence)
+}
+
+sum(apply(y, 1, max)) # observed number of occupied sites with at least one detection)
+
+# Plot observed data and true effect of wind on detect probability
+
+plot(wind, y, xlab="Wind", ylab="Observed det./nondetection data (y)", frame = F, ces = 1.5)
+plot(function (x) plogis(alpha0 + alpha1*x), -1, 1, add = T, lwd = 3, col = "red")
+
+# Look at the data: occupancy, tru presence/absence (z), and measurements (y)
+
+cbind(psi = round(psi, 2), z = z, y1 = y[,1], y2 = y[,2], y3 = y[,3]) # first 6 sites (rows) of the data set
+
+# Create factors
+time <- matrix(rep(as.character(1:J), M), ncol = J, byrow = T) # time factor
+hab <- c(rep("A", 33), rep("B", 33), rep("C", 34)) # Must have M = 100
+
+# Load unmarked, format data and summarize
+
+library(unmarked)
+
+umf <- unmarkedFrameOccu(y = y, # Pres/Abs meausrements
+                         siteCovs = data.frame(vegHt = vegHt, hab = hab), # site-specific covariates
+                         obsCovs = list(wind = wind, time = time)) # obs-specific covariates
+
+summary(umf) # Summarize the data
+
+# Fit the model extract estimates
+# Detection covariates follow first tilde, then occupancy covariates
+
+summary(fm1.occ <- occu(~ wind ~ vegHt, data = umf)) # Fit the model
+
+# Predict occupancy as function of covs (with 95% CIs)
+
+# Add truth from data simulation (below for full code to produce fig. 10-2)
+
+newdat <- data.frame(vegHt = seq(-1, 1, 0.01))
+
+pred.occ <- predict (fm1.occ, type = "state", newdata = newdat)
+
+newdat <- data.frame(wind = seq(-1, 1, 0.1))
+
+pred.det <- predict(fm1.occ, type = "det", newdata = newdat)
+
+# Predictions for specified values of vegHt, say 0.2 and 2.1
+
+newdat <- data.frame(vegHt = c(0.2, 2.1))
+
+predict(fm1.occ, type = "state", newdata = newdat, append = T)
+
+# for values of wind of -1 to 1
+
+newdat <- data.frame(wind=seq(-1, 1, 0.5))
+
+predict(fm1.occ, type ="det", newdata = newdat, append = T)
+
+# Fit detection-naive GLM to observed occurrence and plot comparison
+
+summary(fm.glm <- glm(apply(y, 1, max) ~ vegHt, family = binomial))
+
+plot(vegHt, apply(y, 1, max), xlab = "Vegetation height", 
+     ylab = "Observed occurrence ('ever observed?;)", frame = F, cex = 1.5)
+
+plot(function(x) plogis(beta0 + beta1*x), -1, 1, add = T, lwd = 3, col = "red")
+
+lines(vegHt, predict(fm.glm,,"response"), type = "l", lwd = 3)
+
+lines(vegHt, predict(fm1.occ, type = "state")[,1], col="blue", lwd = 3)
+
+legend(-1, 0.9, c("Truth", "'LR' with p", "LR without p"), col = c("red", "blue", "black"), lty = 1, lwd = 3, cex = 1.2)
+
+ranef(fm1.occ)
+
+(psi1 <- predict(fm1.occ, type="state")[1,1])
+
+(p1 <- predict(fm1.occ, type="det")[c(1:3),1])
+
+(z1 <- (psi1*prod(1 - p1)) / ((1 - psi1) + psi1*prod(1 - p1)))
+
+# Define the function for finite-sample number and proportion of occupied sites
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
